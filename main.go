a2fd54b02179bc378aec96df7b31e9c75102ee6f
takokun778 <takokun.778@gmail.com>
2022-07-24 17:39:38 +0900

    chapter 16

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"todo/config"
 
 	"golang.org/x/sync/errgroup"
@@ -31,6 +33,9 @@ func main() {
 }
 
 func run(ctx context.Context) error {
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
 	cfg, err := config.New()
 	if err != nil {
 		return err
