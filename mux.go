6df106a1429f317f34c25903d6576cec201cf20a
takokun778 <takokun.778@gmail.com>
2022-07-24 18:02:29 +0900

    chapter 16

---
+++ mux.go
@@ -0,0 +1,15 @@
+package main
+
+import "net/http"
+
+func NewMux() http.Handler {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset-utf-8")
+
+		_, _ = w.Write([]byte(`{"status": "OK"}`))
+	})
+
+	return mux
+}
